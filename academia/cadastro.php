02db32cedc28f5cae5997032ac8c7260d26d2c9e
Rosa <164552124+rosamrcl@users.noreply.github.com>
2025-02-25 14:51:20 -0300

    .

--- academia/cadastro.php
+++ academia/cadastro.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,12 +8,15 @@
     <link rel="stylesheet" href="./css/cadastro.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Days+One&family=Gugi&family=Quicksand:wght@300..700&family=Road+Rage&family=Sigmar&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Days+One&family=Gugi&family=Quicksand:wght@300..700&family=Road+Rage&family=Sigmar&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 </head>
+
 <body>
-<header>
-        
+    <header>
+
         <div class="menu-content">
             <img class="logoimg" src="./img/image_1-removebg-preview.png" alt="">
             <nav class="headermenu">
@@ -21,120 +25,121 @@
                     <li><a href="plano.html">Planos</a></li>
                     <li><a href="imc.php">Calcule seu IMC</a></li>
                     <li><a href="cadastro.php">Cadastro</a></li>
-                    <li><a href="contato.html"><img class="socialmedia" src="./img/redes-sociais.png" alt=""></a></li>                        
+                    <li><a href="contato.html"><img class="socialmedia" src="./img/redes-sociais.png" alt=""></a></li>
                     <li><a href="sobrenos.html">Sobre Nós</a></li>
                 </ul>
-                
+
 
             </nav>
         </div>
-        
-    
-
-</header>
-<main>
-    <div class="maincontent">
-        <h1 class="primary-text">Malhar é questão de saúde física e mental</h1>
-        <h2 class="second-text">
-        SE INSCREVA AGORA E MUDE DE VIDA
-        </h2>   
-    
-        <form action="resposta.php" method="post">
-            <fieldset> <legend><h1 class="primary-text">Cadastro do Aluno</h1></legend><br>
-                <div class="inputbox">
-                    <input type="text" name="nome" id="nome" class="inputUser" required>
-                    <label for="nome" class="labelinput">Nome</label>                    
-                </div><br>
-                <div class="inputbox" >
-                    <input type="text" name="lastaname" id="lastname" class="inputUser" required>
-                    <label for="lastname"class="labelinput">Sobrenome</label>
-                </div><br>
-                <div class="inputbox">
-                    <input type="email" name="email" id="email" class="inputUser" required>
-                    <label for="email"class="labelinput">E-mail</label>
-                </div><br>
-                <div class="inputbox">
-                    <input type="tel" name="tel" id="tel" class="inputUser">
-                    <label for="tel"class="labelinput">Telefone</label>
-                </div><br>
-                <p>Sexo:</p>
-                <input type="radio" name="genero" id="feminino" value="feminino" required>
-                <label for="feminino">Feminino</label><br>
-                <input type="radio" name="genero" id="masculino" value="masculino" required>
-                <label for="masculino">Masculino</label><br>
-                <input type="radio" name="genero" id="outro" value="outro" required>
-                <label for="outro">Outro</label><br>
-
-                <div class="inputbox">
-                    <br><label for="data_nascimento">Data de nascimento</label>
-                    <input type="date" name="data_nascimento" id="data_nascimento" required><br>
-                    
-                    
-
-                </div><br>
-                <div class="inputbox">
-                    <input type="number" step="0.01" name="peso"  id="peso" class="inputUser" required><br>
-                    <label for="peso"  class="labelinput">Peso</label><br>
-                </div><br>
-                <div class="inputbox">                        
-                    <input type="number" step="0.01" name="altura" id="altura" class="inputUser" required><br>
-                    <label for="altura" class="labelinput">Altura</label>               
-                </div><br>
-                
-    
-                <div class="inputbox">
-                    <input type="text" name="adress" id="adress" class="inputUser">
-                    <label for="adress" class="labelinput">Endereço</label>
-                </div><br>
-                <div class="inputbox">
-                    <input type="number" name="number" id="number" class="inputUser">
-                    <label for="number"class="labelinput">N°</label>
-                </div><br>
-                <div class="inputbox">
-                    <input type="text" id="complemento" name="complemento" class="inputUser">
-                    <label for="complemento" class="labelinput">Complemento</label>
-                </div><br>
-                <div class="inputbox">
-                    <input type="text" id="cidade" name="cidade" class="inputUser">
-                    <label for="cidade" class="labelinput">Cidade</label>
-                </div><br>
-                <div class="inputbox">
-                    <input type="text" id="estado" name="estado" class="inputUser">
-                    <label for="estado" class="labelinput">Estado</label>
-                </div><br>
-                <div class="inputbox">
-                    <input type="number" name="cep" id="cep" class="inputUser">
-                    <label for="cep" class="labelinput">CEP</label>
-                </div><br>
-                <div class="inputbox">
-                    <form action="./post=true" method="post" name="post" enctype="multipart/form-data">
-                        <input type="file" name="file" id="file" class="inputUser">
-                    
-                    </form>
-                </div><br>
-                <div class="inputbox">
-                    <input type="datetime-local" name="datalocal" id="datalocal" >
-                    <label for="datalocal" >Data e local</label>
-
-                </div><br>
-                <input type="submit" value="Enviar" id="enviar" class="btns">
-                
-                
-
-            </fieldset>
 
 
-        </form>
-    </div>
 
-    
-</main>
-<footer>
+    </header>
+    <main>
+        <div class="maincontent">
+            <h1 class="primary-text">Malhar é questão de saúde física e mental</h1>
+            <h2 class="second-text">
+                SE INSCREVA AGORA E MUDE DE VIDA
+            </h2>
+
+            <form action="resposta.php" method="post">
+                <fieldset>
+                    <legend>
+                        <h1 class="primary-text">Cadastro do Aluno</h1>
+                    </legend><br>
+                    <div class="inputbox">
+                        <input type="text" name="nome" id="nome" class="inputUser" required>
+                        <label for="nome" class="labelinput">Nome</label>
+                    </div><br>
+                    <div class="inputbox">
+                        <input type="text" name="lastaname" id="lastname" class="inputUser" required>
+                        <label for="lastname" class="labelinput">Sobrenome</label>
+                    </div><br>
+                    <div class="inputbox">
+                        <input type="email" name="email" id="email" class="inputUser" required>
+                        <label for="email" class="labelinput">E-mail</label>
+                    </div><br>
+                    <div class="inputbox">
+                        <input type="tel" name="tel" id="tel" class="inputUser">
+                        <label for="tel" class="labelinput">Telefone</label>
+                    </div><br>
+                    <p>Sexo:</p>
+                    <input type="radio" name="genero" id="feminino" value="feminino" required>
+                    <label for="feminino">Feminino</label><br>
+                    <input type="radio" name="genero" id="masculino" value="masculino" required>
+                    <label for="masculino">Masculino</label><br>
+                    <input type="radio" name="genero" id="outro" value="outro" required>
+                    <label for="outro">Outro</label><br>
+
+                    <div class="inputbox">
+                        <br><label for="data_nascimento">Data de nascimento</label>
+                        <input type="date" name="data_nascimento" id="data_nascimento" required><br>
+
+
+
+                    </div><br>
+                    <div class="inputbox">
+                        <input type="number" step="0.01" name="peso" id="peso" class="inputUser" required><br>
+                        <label for="peso" class="labelinput">Peso</label><br>
+                    </div><br>
+                    <div class="inputbox">
+                        <input type="number" step="0.01" name="altura" id="altura" class="inputUser" required><br>
+                        <label for="altura" class="labelinput">Altura</label>
+                    </div><br>
+
+
+                    <div class="inputbox">
+                        <input type="text" name="adress" id="adress" class="inputUser">
+                        <label for="adress" class="labelinput">Endereço</label>
+                    </div><br>
+                    <div class="inputbox">
+                        <input type="number" name="number" id="number" class="inputUser">
+                        <label for="number" class="labelinput">N°</label>
+                    </div><br>
+                    <div class="inputbox">
+                        <input type="text" id="complemento" name="complemento" class="inputUser">
+                        <label for="complemento" class="labelinput">Complemento</label>
+                    </div><br>
+                    <div class="inputbox">
+                        <input type="text" id="cidade" name="cidade" class="inputUser">
+                        <label for="cidade" class="labelinput">Cidade</label>
+                    </div><br>
+                    <div class="inputbox">
+                        <input type="text" id="estado" name="estado" class="inputUser">
+                        <label for="estado" class="labelinput">Estado</label>
+                    </div><br>
+                    <div class="inputbox">
+                        <input type="number" name="cep" id="cep" class="inputUser">
+                        <label for="cep" class="labelinput">CEP</label>
+                    </div><br>
+                    <div class="inputbox">
+                        <form action="./post=true" method="post" name="post" enctype="multipart/form-data">
+                            <input type="file" name="file" id="file" class="inputUser">
+
+                        </form>
+                    </div><br>
+                    <div class="inputbox">
+                        <input type="datetime-local" name="datalocal" id="datalocal">
+                        <label for="datalocal">Data e local</label>
+
+                    </div><br>
+                    <input type="submit" value="Enviar" id="enviar" class="btns">
+
+
+
+                </fieldset>
+
+
+            </form>
+        </div>
+
+
+    </main>
+    <footer>
         <a href="https://github.com/RosaCL"><img class="consti" src="./img/costi.svg" alt=""></a>
     </footer>
 
-</body> 
+</body>
 
 </html>
-
-    
--- academia/resposta.php
+++ academia/resposta.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,13 +8,16 @@
     <link rel="stylesheet" href="./css/cadastro.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Days+One&family=Gugi&family=Quicksand:wght@300..700&family=Road+Rage&family=Sigmar&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Days+One&family=Gugi&family=Quicksand:wght@300..700&family=Road+Rage&family=Sigmar&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 </head>
+
 <body>
     <main>
-<header>
-        
+        <header>
+
             <div class="menu-content">
                 <img class="logoimg" src="./img/image_1-removebg-preview.png" alt="">
                 <nav class="headermenu">
@@ -22,20 +26,23 @@
                         <li><a href="plano.html">Planos</a></li>
                         <li><a href="imc.php">Calcule seu IMC</a></li>
                         <li><a href="cadastro.php">Cadastro</a></li>
-                        <li><a href="contato.html"><img class="socialmedia" src="./img/redes-sociais.png" alt=""></a></li>                        
+                        <li><a href="contato.html"><img class="socialmedia" src="./img/redes-sociais.png" alt=""></a>
+                        </li>
                         <li><a href="sobrenos.html">Sobre Nós</a></li>
                     </ul>
-                    
+
 
                 </nav>
             </div>
-            
-        
 
-    </header>
+
+
+        </header>
         <div class="maincontent">
             <fieldset>
-                <legend> <h1>Cadastro do aluno</h1></legend>
+                <legend>
+                    <h1>Cadastro do aluno</h1>
+                </legend>
                 <?php 
                 if ($_SERVER['REQUEST_METHOD']=="POST"){
 
@@ -77,7 +84,7 @@
                 }
                 
                 ?>
-                
+
                 <?php
                 $folder=__DIR__ ."/uploads/";
                 if(!file_exists($folder)||!is_dir($folder)){
@@ -108,17 +115,18 @@
                             }
                         }
                         var_dump(scandir(__DIR__ ."/uploads"));
-                    ?>                                 
+                    ?>
 
             </fieldset>
         </div>
 
-        
-        
+
+
     </main>
     <footer>
         <a href="https://github.com/RosaCL"><img class="consti" src="./img/costi.svg" alt=""></a>
     </footer>
-    
+
 </body>
+
 </html>
