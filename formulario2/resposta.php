6af37e6cc7058697fb7fcbc39278184c547a6635
Rosa <costurezati@gmail.com>
2025-02-26 09:05:36 -0300

    .

--- formulario2/resposta.php
+++ formulario2/resposta.php
@@ -14,36 +14,34 @@
             <fieldset>
                 <legend>Dados Recebidos</legend>
                 <?php 
-                 if($_SERVER['REQUEST_METHOD']=="POST"){
+                    if($_SERVER['REQUEST_METHOD']=="POST"){
 
-                     $nome=$_POST["nome"];
-                     $lastaname=$_POST["lastaname"];
-                     $email=$_POST["email"];
-                     $tel=$_POST["tel"];
-                     $adress=$_POST["adress"];
-                     $number=$_POST["number"];
-                     $complemento=$_POST["complemento"];
-                     $cidade=$_POST["cidade"];
-                     $estado=$_POST["estado"];
-                     $cep=$_POST["cep"];
-                     
-                     echo "<p><strong>Nome:</strong>$nome</p>";
-                     echo "<p><strong>Sobrenome:</strong>$lastaname</p>";                
-                     echo "<p><strong>E-mail:</strong>$email</p>";
-                     echo "<p><strong>Telefone:</strong>$tel</p>";
-                     echo "<p><strong>Endereço:</strong>$adress</p>";
-                     echo "<p><strong>Nº:</strong>$number</p>";
-                     echo "<p><strong>Complemento:</strong>$complemento</p>";
-                     echo "<p><strong>Cidade:</strong>$cidade</p>";
-                     echo "<p><strong>Estado:</strong>$estado</p>";
-                     echo "<p><strong>CEP:</strong>$cep</p>";
-                 }else{
+                        $nome=$_POST["nome"];
+                        $lastaname=$_POST["lastaname"];
+                        $email=$_POST["email"];
+                        $tel=$_POST["tel"];
+                        $adress=$_POST["adress"];
+                        $number=$_POST["number"];
+                        $complemento=$_POST["complemento"];
+                        $cidade=$_POST["cidade"];
+                        $estado=$_POST["estado"];
+                        $cep=$_POST["cep"];
+                        
+                        echo "<p><strong>Nome:</strong>$nome</p>";
+                        echo "<p><strong>Sobrenome:</strong>$lastaname</p>";                
+                        echo "<p><strong>E-mail:</strong>$email</p>";
+                        echo "<p><strong>Telefone:</strong>$tel</p>";
+                        echo "<p><strong>Endereço:</strong>$adress</p>";
+                        echo "<p><strong>Nº:</strong>$number</p>";
+                        echo "<p><strong>Complemento:</strong>$complemento</p>";
+                        echo "<p><strong>Cidade:</strong>$cidade</p>";
+                        echo "<p><strong>Estado:</strong>$estado</p>";
+                        echo "<p><strong>CEP:</strong>$cep</p>";
+                    }else{
                     header("Location:index.php");
                     exit();
-                 }
-                 
-                
-                
+                    }
+
                 ?>
             </fieldset>
         </div>
