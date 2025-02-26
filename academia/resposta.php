9870792f5dfcb4b9adc03d25fa518425abb69ade
Rosa <164552124+rosamrcl@users.noreply.github.com>
2025-02-26 13:22:59 -0300

    .

--- academia/resposta.php
+++ academia/resposta.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro do aluno</title>
-    <link rel="stylesheet" href="./css/cadastro.css">
+    <link rel="stylesheet" href="./css/cadastroresposta.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
